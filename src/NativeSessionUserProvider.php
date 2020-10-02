0a45bc521e13f5558f28d33b8be8f09a95df406a
Deleu <deleugyn@gmail.com>
2020-10-02 15:12:00 +0200

    Basic, initial setup

---
+++ src/NativeSessionUserProvider.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace CustomerGauge\Session;
+
+use App\Components\Authentication\Authorization;
+use App\Components\Authentication\User;
+use Illuminate\Contracts\Auth\Authenticatable as LaravelAuthenticatable;
+use Illuminate\Contracts\Auth\UserProvider;
+
+final class NativeSessionUserProvider implements UserProvider
+{
+    private $factory;
+
+    public function __construct(UserFactory $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function retrieveByCredentials(array $session)
+    {
+        return $this->factory->make($session);
+    }
+
+    /** @phpstan-ignore-next-line */
+    public function retrieveById($identifier)
+    {}
+
+    /** @phpstan-ignore-next-line */
+    public function retrieveByToken($identifier, $token)
+    {}
+
+    public function updateRememberToken(LaravelAuthenticatable $user, $token)
+    {}
+
+    /** @phpstan-ignore-next-line */
+    public function validateCredentials(LaravelAuthenticatable $user, array $credentials)
+    {}
+}
